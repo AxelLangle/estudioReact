308113f1bd35a96ae08bae2e21ce7c819347d54a
AxelLangle <axellangle40@gmail.com>
2025-10-16 15:27:37 -0600

    Se hizo la twitter follow card

---
+++ projects/hola mundo/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://yt3.googleusercontent.com/J-FzOKKYj-Rp7oEMPqcHMMzxyDSDqhFfZN3onmK4kKjIaoV2Ljxq5tHGOH5GYlmuyk8pzbcE=s160-c-k-c0x00ffffff-no-rj" alt="El avatar de MiduDev" />
+        <div>
+          <strong>Axel Pene Gordo Langle</strong><br />
+          <span>Desarrollador Web</span>
+        </div>
+        <aside>
+          <button>Seguir</button>
+        </aside>
+      </header>
+    </article>
+  )
+}
+
